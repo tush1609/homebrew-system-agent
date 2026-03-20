1106e8d5861e950fb7250b176c389559cf529aa2
Tushar Verma <tushar.gkp1609@gmail.com>
2026-03-21 03:42:03 +0530

    added orjson

--- Formula/system-agent.rb
+++ Formula/system-agent.rb
@@ -14,10 +14,11 @@ class SystemAgent < Formula
     ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
     ENV["PIP_NO_BINARY"] = "cryptography"
     ENV["LDFLAGS"] = "-Wl,-headerpad_max_install_names"
+    ENV["RUSTFLAGS"] = "-C link-arg=-headerpad_max_install_names"
 
     system "python3.11", "-m", "venv", libexec
     system libexec/"bin/python", "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"
-    system libexec/"bin/python", "-m", "pip", "install", "--no-cache-dir", "--no-binary", "jiter,orjson", "-r", "requirements.txt"
+    system libexec/"bin/python", "-m", "pip", "install", "--no-cache-dir", "--no-binary", "jiter,orjson,ormsgpack", "-r", "requirements.txt"
 
     libexec.install Dir["*"]
 
