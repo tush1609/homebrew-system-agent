36af791fb6bcb72367b5694227d055b8acc0aa98
Tushar Verma <tushar.gkp1609@gmail.com>
2026-03-21 02:27:23 +0530

    ruby file updated

--- Formula/system-agent.rb
+++ Formula/system-agent.rb
@@ -4,7 +4,7 @@ class SystemAgent < Formula
   desc "Lightweight multi-agent chat orchestrator with Chainlit UI"
   homepage "https://github.com/tush1609/system_agent"
   url "https://github.com/tush1609/system_agent/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "40ed5bc2de762d68fb3c21f26e54c3299ebddec915c3e7bb92b357bffb165d9c"
+  sha256 "4255004d066b6559cc96caca90b3f3077e47d5b8fc59dad5694c9f17d8dc22de"
   license "NOASSERTION"
 
   depends_on "python@3.11"
